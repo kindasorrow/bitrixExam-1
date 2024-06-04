9050e19fef98113fd4f6b30abe1b2d71361d0c62
kindasorrow <88721658+kindasorrow@users.noreply.github.com>
2024-06-04 11:15:33 +0500

    Delete bitrix/managed_cache/MYSQL directory

--- bitrix/managed_cache/MYSQL/user_option/e4/e46a7cd1240e5249e769c3b594014c74.php
+++
@@ -1,7 +0,0 @@
-<?
-if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001717408728';
-$dateexpire = '001717412328';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{s:18:"file_dialog_config";s:27:"s1;/partneram;list;type;asc";}}';
-return true;
-?>
